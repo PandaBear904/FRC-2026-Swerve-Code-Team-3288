a2674c5aa0b4a46a481e64f059b94e5b909822e6
Scotty Ring <124225880+PandaBear904@users.noreply.github.com>
2026-01-20 14:38:15 -0700

    Updated build.gradle and added Constants.java

---
+++ src/main/java/frc/robot/Constants.java
@@ -0,0 +1,19 @@
+package frc.robot;
+
+public final class Constants {
+
+    public final class shootingConstants{
+        public final int shootingMotorID = 0;
+        public final int followingShootingMotorID = 0;
+
+        public final double maxShootingV = 0;
+    }
+
+    public final class climbingConstants{
+        public final int climbingMotorID = 0;
+        public final int followingClimbingMotorID = 0;
+
+        public final double maxClimbingV = 0;
+    }
+    
+}
