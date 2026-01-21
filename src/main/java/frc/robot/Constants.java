c9122f3897cb47744e78747cb875b45747f9e99d
Scotty Ring <124225880+PandaBear904@users.noreply.github.com>
2026-01-20 20:06:20 -0700

    Added Constants.java, ShootingSubsytem.java and REVLib.json

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -2,18 +2,19 @@ package frc.robot;
 
 public final class Constants {
 
-    public final class shootingConstants{
-        public final int shootingMotorID = 0;
-        public final int followingShootingMotorID = 0;
+    public static class ShootingConstants{
+        public static int shootingMotorID = 0;
+        public static int followingShootingMotor1ID = 0;
 
-        public final double maxShootingV = 0;
+        public static final int maxShootingV = 0;
+        public static final int spinUpShootingV = 0;
     }
 
-    public final class climbingConstants{
-        public final int climbingMotorID = 0;
-        public final int followingClimbingMotorID = 0;
+    public static class ClimbingConstants{
+        public static int climbingMotorID = 0;
+        public static int followingClimbingMotorID = 0;
 
-        public final double maxClimbingV = 0;
+        public static double maxClimbingV = 0;
     }
     
 }
